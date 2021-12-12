9817b269ce99604aaf9f5d707d43671677707e01
Vardan <vardan.vanyan@gmail.com>
2021-12-11 19:40:20 -0800

    added a package called body-parser and used it to do server side calculations

--- calculator.js
+++ calculator.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  let num1 = Number(req.body.n1);
+  let num2 = Number(req.body.n2);
+
+  let result = num1 + num2;
+  res.send(`The result of the calculation is ${result}`);
 });
 
 app.listen(port, function () {
-  console.log("This Server is now running!");
+  console.log("This Server is now running on port 3000");
 });
