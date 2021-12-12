d98a49e1faddde39a62d666694bf16b45912953e
Vardan <vardan.vanyan@gmail.com>
2021-12-11 20:11:47 -0800

    added a bmi calculator doing server side calculations

--- calculator.js
+++ calculator.js
@@ -9,6 +9,21 @@ const port = 3000;
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+//bmi calc get
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+//bmi calc post
+app.post("/bmicalculator", function (req, res) {
+  let weight = Number(req.body.weight);
+  let height = Number(req.body.height);
+
+  let bmi = (weight / height / height) * 703;
+
+  console.log(weight, height);
+  res.send(`Your BMI is: ${Math.round(bmi)}`);
+});
 
 app.post("/", function (req, res) {
   let num1 = Number(req.body.n1);
